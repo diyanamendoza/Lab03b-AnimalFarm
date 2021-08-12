cdd30b4f64e785610913ae4f3337f3e34d07e6c0
diyanamendoza <diyana.amp@pm.me>
2021-08-11 17:32:35 -0700

    Add header function

--- app.js
+++ app.js
@@ -7,15 +7,22 @@ const dogImage = document.getElementById('dog-pic');
 const catImage = document.getElementById('cat-pic');
 const horseImage = document.getElementById('horse-pic');
 
+const animalHeard = document.getElementById('animal-heard');
+
 // set event listeners 
 dogImage.addEventListener('click', () => {
   dogSound.play();
+  animalHeard.textContent = "Dog";
 })
 
 catImage.addEventListener('click', () => {
   catSound.play();
+  animalHeard.textContent = "Cat";
+
 })
 
 horseImage.addEventListener('click', () => {
   horseSound.play();
+  animalHeard.textContent = "Horse";
+
 })
